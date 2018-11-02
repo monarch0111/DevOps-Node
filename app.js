56226f6f18a536951cb5f434a4e09dca245413b5
Abhishek <abhishek@box8.in>
2018-11-02 13:00:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', function(req, res) {
+app.get('/now', function(req, res) {
 	return res.send({current_time: new Date()})
 })
 
